8d595ce2904314d32fceea78779ec065909aa8b9
jeffart <dekogha@gmail.com>
2012-12-16 14:30:45 +0100

    go

--- config.php
+++ config.php
@@ -7,10 +7,18 @@
         include 'mysqli.class.php';
 
         $config = array();
-		$config['host'] = 'db446462646.db.1and1.com';
-		$config['user'] = 'dbo446462646';
-		$config['pass'] = 'fidelejesus';
-		$config['table'] = 'db446462646';
+		$config['host'] = 'localhost';
+		$config['user'] = 'chat';
+		$config['pass'] = 'chat';
+		$config['table'] = 'dc_livechat';
 		
-		// ici on se connecte
+		
+		
+		
+	//$config = array();
+	//	$config['host'] = 'db446462646.db.1and1.com';
+	//	$config['user'] = 'dbo446462646';
+	//	$config['pass'] = 'fidelejesus';
+	//	$config['table'] = 'db446462646';
+	
  		$db = new DB($config);
--- index.php
+++ index.php
@@ -13,55 +13,56 @@
 	
 	<div> Hello User</div>
 	
-	<?php 
-    $ip = $_SERVER['REMOTE_ADDR'];
-    $ip_data = get_ip_info($ip);
-    echo('Hostname ### ' . $ip_data['host'] . '<br/>');
-    echo('IP Country ### ' . $ip_data['country'] . '<br/>');
-    echo('IP Country Code ### ' . $ip_data['country_code'] . '<br/>');
-    echo('IP Continent ### ' . $ip_data['continent'] . '<br/>');
-    echo('IP Region ### ' . $ip_data['region'] . '<br/>');
-    echo('IP Latitude ### ' . $ip_data['latitude'] . '<br/>');
-    echo('IP Longitude ### ' . $ip_data['longitude'] . '<br/>');
-    echo('Organization ### ' . $ip_data['organization'] . '<br/>');
-    echo('ISP Provider ### ' . $ip_data['isp']);
+	<?php # Script 11.3 - ip_geo.php
+	
+	// This page uses a Web service to retrieve a user's geographic location.
 
-    function get_ip_info($ip = NULL) 
-    { 
-       if(empty($ip)) return false; 
-       $ch = curl_init(); 
-       curl_setopt($ch, CURLOPT_URL, 'http://www.ipaddresslocation.org/ip-address-locator.php'); 
-       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-       curl_setopt($ch, CURLOPT_POST, true); 
-       curl_setopt($ch, CURLOPT_POSTFIELDS, array('ip' => $ip)); 
-       $data = curl_exec($ch); 
-       curl_close($ch);     
-       preg_match_all('/<i>([a-z\s]+)\:<\/i>\s+<b>(.*)<\/b>/im', $data, $matches, PREG_SET_ORDER); 
-       if(count($matches) == 0) return false;    
-       $return = array(); 
-       $format_labels = array(
-            'Hostname'          => 'host', 
-            'IP Country'        => 'country', 
-            'IP Country Code'   => 'country_code', 
-            'IP Continent'      => 'continent', 
-            'IP Region'         => 'region', 
-            'IP Latitude'       => 'latitude', 
-            'IP Longitude'      => 'longitude', 
-            'Organization'      => 'organization', 
-            'ISP Provider'      => 'isp' 
-       ); 
-       foreach($matches as $info) 
-       { 
-          if(isset($info[2]) && !is_null($format_labels[$info[1]])) 
-          { 
-             $return[$format_labels[$info[1]]] = $info[2]; 
-          } 
-       }
-       return (count($return)) ? $return : false; 
-    }
-?>
+	// This function will perform the IP Geolocation request:
+	function show_ip_info($ip) {
+		// Identify the URL to connect to:
+		$url = '#';
+		//http://www.ipaddresslocation.org/ip-address-locator.php
+		$curl = curl_init($url);
+		
+		 // Open the connection:
+		// $fp = fopen($url, 'r');
+		 
+		
+		// Allow for redirects:
+      // $d =  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		
+		var_dump($curl) ;
+		
+		//echo  $d;
+		 
+		// Execute the transaction:
+        $r = curl_exec($curl);
+
+   // Close the connection:
+   curl_close($curl);
+
+  // Print the results:
+   print_r($r);
+		 
+		 // Print whatever about the IP:
+		 echo "<p>IP Address: $ip<br>
+		 Country: $read[2]<br>
+		 City, State: $read[5], $read[3]<br>";
+	
+	} // End of show_ip_info() function.
+	
+	
+	?>
+	
+	<?php
+	echo '<h2> Ip information </h2>';
+	
+	
+    show_ip_info($_SERVER ['REMOTE_ADDR']);
+	
 	
 
+	?>
 	
 	
 	 <script>
